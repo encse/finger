849a9202fb0b93db163173bda432b38d1b48639a
David Nemeth Csoka <encse@csokavar.hu>
2022-11-19 08:39:16 +0100

    stats

--- src/blocks/banner.ts
+++ src/blocks/banner.ts
@@ -7,9 +7,15 @@ import config from '../../config.json';
 
 export function banner(): string {
 
-    let msg = fs.readFileSync('public/logo.txt', {encoding: 'utf8'});
+    let msg ="Pshhhkkkkkkrrrr​kakingkakingkakingtsh​chchchchchchchcch​*ding*ding*ding*....\n\n\n\n";
+
+    msg += fs.readFileSync('public/logo.txt', {encoding: 'utf8'});
+    const loadAvg = os.loadavg().map(x => x.toFixed(2)).join(' ')
+
+    msg += "\n";
     msg += "\n";
     msg += center("This is csokavar.hu Encse's home on the web. Happy surfing.", config.screen_width) + "\n";
+    msg += center(`${os.arch()} ${os.platform()}, cpus: ${os.cpus().length}, load average: ${loadAvg}`, config.screen_width) + "\n";
     msg += center("Server uptime: " + uptime(), config.screen_width) + "\n";
     msg += center("contact: encse@csokavar.hu", config.screen_width) + "\n";
     msg += "\n";
@@ -18,19 +24,18 @@ export function banner(): string {
 }
 
 function uptime() {
-    var ut_sec = os.uptime();
-    var ut_min = ut_sec / 60;
-    var ut_hour = ut_min / 60;
+    let ut_sec = os.uptime();
+    let ut_min = ut_sec / 60;
+    let ut_hour = ut_min / 60;
+    let ut_day = ut_hour / 24;
 
-    ut_sec = Math.floor(ut_sec);
-    ut_min = Math.floor(ut_min);
-    ut_hour = Math.floor(ut_hour);
+    ut_sec = Math.floor(ut_sec) % 60;
+    ut_min = Math.floor(ut_min) % 60;
+    ut_hour = Math.floor(ut_hour) % 24;
+    ut_day = Math.floor(ut_day);
 
-    ut_hour = ut_hour % 60;
-    ut_min = ut_min % 60;
-    ut_sec = ut_sec % 60;
-
-    return (ut_hour + " Hour(s) "
+    return (ut_day + " Days(s) "
+        + ut_hour + " Hour(s) "
         + ut_min + " minute(s) and "
         + ut_sec + " second(s)");
 }
