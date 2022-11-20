a6e6e8963f37c8ec1f1c1f263692a9dab947512a
David Nemeth Csoka <encse@csokavar.hu>
2022-11-20 07:01:37 +0100

    move chunks to the server side

--- src/services/http.ts
+++ src/services/http.ts
@@ -3,6 +3,19 @@ import { getMessage } from "../blocks/message";
 import { server as WebSocketServer } from 'websocket';
 import http from 'http';
 
+function sleep(ms: number) { 
+    return new Promise((r) => setTimeout(r, ms));
+}
+
+function* randomChunks(st: string) {
+    let chunkLength = 0;
+
+    for (let i = 0; i < st.length; i += chunkLength) {
+        chunkLength = Math.floor(Math.random() * 500);
+        yield st.substring(i, i + chunkLength);
+    }
+}
+
 export function httpService(http_port: number) {
     const app = express();
     app.use(express.static('public'))
@@ -14,7 +27,11 @@ export function httpService(http_port: number) {
         connection.on('error', (err) => {
             console.error("websocket connection error", err);
         })
-        connection.sendUTF(await getMessage());
+        let message = await getMessage();
+        for (let chunk of randomChunks(message)) {
+            connection.sendUTF(chunk);
+            await sleep(50);
+        }
         connection.close();
     });
 
