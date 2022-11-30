32c056e72ae213c14f80d3b328d0ee8f579a5527
David Nemeth Csoka <encse@csokavar.hu>
2022-11-30 19:27:36 +0100

    fix a readline handling error

--- src/services/finger.ts
+++ src/services/finger.ts
@@ -4,23 +4,30 @@ import * as readline from 'readline';
 import { getFingerMessage } from '../blocks/finger';
 import { lookupUser } from '../server/users';
 
-export function fingerService(finger_port: number){
-    const server = net.createServer(async (socket) => {
-        const rl = readline.createInterface({ input:socket });
-        rl.on('line', async (line: string) => {
+export function fingerService(finger_port: number) {
+    const server = net.createServer((socket) => {
+        socket.on('error', (err) => { 
+            console.log('socket', err) 
+        });
+
+        readline.createInterface({
+            input: socket,
+        }).on('line', async (line: string) => {
             try {
                 let message = await getFingerMessage(lookupUser(line));
-                message = asciiFold(message);
-                socket.write(message);
-            } catch(err) {
-                console.log(err);
+                socket.write(asciiFold(message));
+            } catch(err){
+                console.log('on:line', err);
             } finally {
                 socket.end();
             }
+        }).on('error', (err) => {
+            console.log('readline', err);
         })
+
     }).on('error', (err) => {
         console.error('finger server error', err);
-    });
+    })
 
     server.listen(finger_port, () => {
         console.log('opened finger server on', server.address());
@@ -28,11 +35,11 @@ export function fingerService(finger_port: number){
 }
 
 function asciiFold(st: string) {
-    st = st.replace(/█/g,'#');
-    st = st.replace(/▀/g,'"');
-    st = st.replace(/▌/g,';');
-    st = st.replace(/▐/g,':');
-    st = st.replace(/▄/g,'.');
+    st = st.replace(/█/g, '#');
+    st = st.replace(/▀/g, '"');
+    st = st.replace(/▌/g, ';');
+    st = st.replace(/▐/g, ':');
+    st = st.replace(/▄/g, '.');
     // remove accents such as á -> a, é -> e, because raw TCP doesn't like it...
     st = st.normalize("NFD").replace(/\p{Diacritic}/gu, "");
 
